5b90569d27b1e5eeff560c34239f93a513a232dd
Tomer A <tomera1@rafael.co.il>
2023-01-17 19:35:04 +0200

    step 1

--- case_studies/aes/count_summary.py
+++ case_studies/aes/count_summary.py
@@ -10,14 +10,12 @@ with open(sys.argv[1]) as file:
         line = line.rstrip("\n").replace(" ","") #remove new line and all spaces
         line_parts = line.split(':')
         lines_list.append(line_parts)
-#my_dict = {i:lines.count(i) for i in lines}
+
 #print(lines_list)
 uniq_items= sorted(set(map(tuple, lines_list)), reverse=True)
 
-print(uniq_items)
-#result_counter = Counter(x[0] for x in lines_list)
-#print(result_counter)
-print(type(uniq_items[0]))
+#print(uniq_items)
+#print(type(uniq_items[0]))
 uniq_items_counter = [0]*len(uniq_items) #create list of 0 as counter  of uniq list
 for item in lines_list:
     #print(tuple(item))
@@ -25,6 +23,13 @@ for item in lines_list:
     #print(ix)
     uniq_items_counter[ix] +=1
 
-print(uniq_items_counter)
+#print(uniq_items_counter)
+#add counter to each item in uniq_items
+merged_list = []
+for counter,item in zip(uniq_items_counter,uniq_items):
+    temp=item+(str(counter),)
+    merged_list.append(temp)
+print(merged_list)
+
         
 
